5465dedd0716c8da3835e4d6e396533b367d677e
Tomasz Szopa <tomszop@gmail.com>
2022-05-21 12:11:19 +0200

    Add image layers in processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -90,7 +90,8 @@
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
-      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);   
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem); 
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);  
 
       //console.log('thisProduct.formInputs:', thisProduct.formInputs);
     }
@@ -121,8 +122,6 @@
       });
   
     }
-    
-  
   
     initOrderForm(){
       const thisProduct = this;
@@ -160,34 +159,37 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        //console.log(paramId);
-
+        console.log(paramId, param);
+  
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          
-
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
-            // check if the option is not default
-            if(option.hasOwnProperty('default') == false) {
+  
+          const optionSelected = (formData[paramId] && formData[paramId].includes(optionId));
+          if(optionSelected) {
+            if (!option.deafult) {
               price += option.price;
             }
-          }else {
-            // check if the option is default
-            if(option.hasOwnProperty('default') == true) {
-              price -= option.price;
+          }
+          else if (option.deafult == true) {
+            price = option.price;
+          }
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+  
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
-          }                
-          //console.log(optionId, option);
+          }
         }
-        
       }
-
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
-      //console.log(thisProduct);
+      thisProduct.imageWrapper.innerHTML;
     }
   }
   
